e0e50aa12bf13deb82425d5d75f6b2bf9ad0ef19
yoojeayoung <yoojeayoung@192.168.0.3>
2017-04-20 00:42:55 +0900

    final

--- oopPracticeProject/src/com/oop/model/dto/Employee.java
+++ oopPracticeProject/src/com/oop/model/dto/Employee.java
@@ -1,6 +1,10 @@
 package com.oop.model.dto;
 
+import java.util.Scanner;
+
 public class Employee {
+	Scanner sc = new Scanner(System.in);
+	
 	private int empNo;
 	private String empName;
 	private String dept;
@@ -13,10 +17,44 @@ public class Employee {
 	private String address;
 	
 	public void empInput(){
-		
+		System.out.print("사원번호 입력 : ");
+		this.empNo = sc.nextInt();
+		System.out.print("사원명 입력 : ");
+		this.empName = sc.next();
+		System.out.print("소속부서 입력 : ");
+		this.dept = sc.next();
+		System.out.print("직급 입력 : ");
+		this.job = sc.next();
+		System.out.print("나이 입력 : ");
+		this.age = sc.nextInt();
+		System.out.print("성별 입력(m/f) : ");
+		this.gender = sc.next().charAt(0);
+		System.out.print("급여 입력 : ");
+		this.salary = sc.nextInt();
+		System.out.print("보너스 포인트 입력 : ");
+		this.bonusPoint = sc.nextDouble();
+		System.out.print("핸드폰 번호 입력 : ");
+		this.phone = sc.next();
+		System.out.print("주소 입력 : ");
+		this.address = sc.nextLine();
+		address = sc.nextLine();
 	}
 	
 	public void empOutput(){
+		System.out.println("사원번호 : " + getEmpNo());
+		System.out.println("사원명 : " + getEmpName());
+		System.out.println("소속부서 : " + getDept());
+		System.out.println("직급 : " + getJob());
+		System.out.println("나이 : " + getAge());
+		System.out.println("성별 : " + (getGender()=='m'?"남자":"여자"));
+		System.out.println("급여 : " + getSalary());
+		System.out.println("보너스포인트 : " + getBonusPoint());
+		System.out.println("전화번호 : " + getPhone());
+		System.out.println("주소 : " + getAddress());
+		
+		
+	}
+	public Employee(){
 		
 	}
 
--- oopPracticeProject/src/com/oop/run/EmpTest.java
+++ oopPracticeProject/src/com/oop/run/EmpTest.java
@@ -5,51 +5,84 @@ import java.util.Scanner;
 import com.oop.model.dto.Employee;
 
 public class EmpTest {
-	
+
 	public static void main(String[] args) {
-		
-
+		mainMenu();
 	}
-	
-	public static void mainMenu(){
+
+	public static void mainMenu() {
+
 		Employee e = new Employee();
-		System.out.println("1. 새 사원 정보 입력");
-		System.out.println("2. 사원 정보 수정");
-		System.out.println("3. 사원 정보 삭제");
-		System.out.println("4. 사원정보 출력");
-		System.out.println("9. 끝내기");
-		
-		int num = new Scanner(System.in).nextInt();
-		
-		switch(num){
-		case 1: e.empInput(); break;
-		case 2: modifyMenu(e); break;
-		case 3: break;
-		case 4: e.empOutput(); break;
-		case 9: break;
-			default : continue;
-		}
-		
-		asdfasdfasdf
-	}
-	
-	public static void modifyMenu(Employee e){
-		System.out.println("1. 이름 변경");
-		System.out.println("2. 급여 변경");
-		System.out.println("3. 부서 변경");
-		System.out.println("4. 직급 변경");
-		System.out.println("5. 이전 메뉴로 이동");
-		
-		int num = new Scanner(System.in).nextInt();
-		
-		
-		switch(num){
-		case 1: e.setEmpName(e.empInput()); break;
-		case 2: modifyMenu(e); break;
-		case 3: break;
-		case 4: e.empOutput(); break;
-		case 5: return;
-			default : continue;
+		char flag;
+
+		do {
+			System.out.println("1. 새 사원 정보 입력");
+			System.out.println("2. 사원 정보 수정");
+			System.out.println("3. 사원 정보 삭제");
+			System.out.println("4. 사원정보 출력");
+			System.out.println("9. 끝내기");
+
+			int num = new Scanner(System.in).nextInt();
+
+			switch (num) {
+			case 1:
+				e.empInput();
+				break;
+			case 2:
+				modifyMenu(e);
+				break;
+			case 3: Employee e1 = new Employee();
+				break;
+			case 4:
+				e.empOutput();
+				break;
+			case 9:
+				System.out.println("정말로 종료하시겠습니까? (y/n)");
+				flag = new Scanner(System.in).next().charAt(0);
+				if (flag == 'y')
+					break;
+			default:
+				continue;
+			}
+		} while (true);
+
 	}
 
+	public static void modifyMenu(Employee e) {
+		Scanner sc = new Scanner(System.in);
+
+		do {
+			System.out.println("1. 이름 변경");
+			System.out.println("2. 급여 변경");
+			System.out.println("3. 부서 변경");
+			System.out.println("4. 직급 변경");
+			System.out.println("5. 이전 메뉴로 이동");
+
+			int num = sc.nextInt();
+
+			switch (num) {
+			case 1:
+				System.out.println("이름을 입력해주세요");
+				e.setEmpName(sc.next());
+				break;
+			case 2:
+				System.out.println("급여를 입력해주세요");
+				e.setSalary(sc.nextInt());
+				break;
+			case 3:
+				System.out.println("소속부서를 입력해주세요");
+				e.setDept(sc.next());
+				break;
+			case 4:
+				System.out.println("직급을 입력해주세요");
+				e.setJob(sc.next());
+				break;
+			case 5:
+				return;
+			default:
+				continue;
+			}
+		} while (true);
+
+	}
 }
