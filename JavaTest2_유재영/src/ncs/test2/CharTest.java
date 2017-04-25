224a09960550d6a80a785a0d99877441ce9cf150
user2 <user2@KH_B>
2017-04-25 15:49:09 +0900

    Final Test

--- JavaTest2_유재영/src/ncs/test2/CharTest.java
+++ JavaTest2_유재영/src/ncs/test2/CharTest.java
@@ -1,21 +1,12 @@
 package ncs.test2;
 
-import java.util.Scanner;
-
 public class CharTest {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		char[] input;
-		int j;
-		
-		System.out.print("문자입력 : ");
-		input = sc.next().toUpperCase().toCharArray();
-
-		for (int i=0; i<input.length; i++){
-			j=(input.length-i)-1;
-			System.out.print(input[j]);
+		char[] input=args[0].toUpperCase().toCharArray();
+						
+		for (int i=input.length-1; i>=0; i--){
+			System.out.print(input[i]);
 		}
 	}
 
