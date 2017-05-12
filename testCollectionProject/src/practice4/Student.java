e995633b38e897cc246b711ba32e813ce8d6d07e
user2 <user2@KH_B>
2017-05-12 21:29:29 +0900

    test

---
+++ testCollectionProject/src/practice4/Student.java
@@ -0,0 +1,7 @@
+package practice4;
+
+public class Student {
+	private int sno;
+	private String sname;
+	private int kor;
+}
---
+++ testCollectionProject/src/practice4/TestProperties.java
@@ -0,0 +1,10 @@
+package practice4;
+
+public class TestProperties {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
